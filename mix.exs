a4eb85e0b8415ae791252c9b3ab2e45773387c3d
dm1try <me@dmitry.it>
2016-11-21 10:00:36 +0300

    up version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MessagePack.RPC.Mixfile do
 
   def project do
     [app: :msgpack_rpc,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      preferred_cli_env: [espec: :test],
      consolidate_protocols: Mix.env != :test,
@@ -17,9 +17,11 @@ defmodule MessagePack.RPC.Mixfile do
   end
 
   defp package do
-     [name: :msgpack_rpc,
+    [name: :msgpack_rpc,
      files: ["lib", "mix.exs", "README*"],
-     licenses: ["Apache 2.0"]]
+     licenses: ["Apache 2.0"],
+     maintainers: ["Dmitry Dedov"],
+     links: %{"GitHub" => "https://github.com/dm1try/msgpack_rpc"}]
   end
 
   defp deps do
